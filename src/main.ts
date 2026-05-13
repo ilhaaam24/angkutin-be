7c9196b986c6f66eaddee48857fcda0f740c5270
Ilham <mi1797128@gmail.com>
2026-05-13 07:05:07 +0700

    feat: initialize application bootstrap with global pipes, interceptors, CORS, and Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -55,6 +55,7 @@ async function bootstrap() {
       'http://localhost:3000',
       'http://127.0.0.1:3000',
       'http://localhost:4000',
+      'https://angkutin-ten.vercel.app'
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
